b04975d7b1236d7d62c3fd6dbe1171c6560726bf
Jin Choi <jinchoi@ucdavis.edu>
2020-07-07 20:57:43 -0700

    DCF-7 added auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const { User } = require('../db/models')
+
+module.exports = async (req, res, next) => {
+  const authHeader = req.header('Authorization')
+
+  if (!authHeader) {
+    return res.send(401)
+  }
+
+  const token = authHeader.replace('Bearer ', '')
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    const user = await User.findOne({ where: { username: decoded.username } })
+
+    if (!user) {
+      throw new Error()
+    }
+
+    next()
+  } catch (e) {
+    res.send(401)
+  }
+}
